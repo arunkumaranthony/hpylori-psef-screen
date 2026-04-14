a697a853cfaec535f7e9c0869440e1b01c1d9d71
Arunkumar Anthony <aa20.arunantony@gmail.com>
2026-04-14 16:37:07 +0530

    v4 pretrained

--- chemprop_train.sh
+++ chemprop_train.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+# Pre-trains the foundation model on the large, general ChEMBL dataset
+
 chemprop train \
   --data-path data/processed/pre_train.csv \
   --task-type classification \
   --smiles-columns smiles \
   --target-columns activity \
   --molecule-featurizers morgan_binary v1_rdkit_2d_normalized \
-  --save-dir models/pretrain_model_v3 \
+  --save-dir models/pretrain_model_v4 \
   --epochs 20
