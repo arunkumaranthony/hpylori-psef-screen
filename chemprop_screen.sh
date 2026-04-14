cc3452dd9a559eeb85410762c7c996c10ccdd379
Arunkumar Anthony <aa20.arunantony@gmail.com>
2026-04-14 16:38:44 +0530

    v4 finetune and screen scripts prepared

--- chemprop_screen.sh
+++ chemprop_screen.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+# Screens the Broad Institute library using the 10-model consensus
+
 chemprop predict \
   -i data/processed/screening_library_final.csv \
-  --model-paths models/finetune_model_v3/model_0/best.pt \
-  -o results/screening_results_v3.csv \
+  --model-paths models/finetune_model_v4/model_*/best.pt \
+  -o results/screening_results_v4.csv \
   --molecule-featurizers morgan_binary v1_rdkit_2d_normalized
