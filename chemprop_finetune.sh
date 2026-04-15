78f3b6410008ab46e4ecbe6858ed1d1316813de5
Arunkumar Anthony <aa20.arunantony@gmail.com>
2026-04-15 13:43:54 +0530

    Trained and fine tuned with extended data

--- chemprop_finetune.sh
+++ chemprop_finetune.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 chemprop train \
-  --data-path data/processed/fine_tune.csv \
+  --data-path data/processed/fine_tune_ext.csv \
   --task-type classification \
   --smiles-columns smiles \
   --target-columns activity \
   --molecule-featurizers morgan_binary v1_rdkit_2d_normalized \
-  --checkpoint models/pretrain_model_v4/model_0/best.pt \
-  --save-dir models/finetune_model_v5_scaffold \
+  --checkpoint models/pretrain_model_ext/model_0/best.pt \
+  --save-dir models/finetune_model_ext_scaffold \
   --epochs 50 \
   --class-balance \
   --split scaffold_balanced \
