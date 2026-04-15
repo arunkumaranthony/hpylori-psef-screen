ead218c202bfecf57cee07f73296d60e88bccca4
Arunkumar Anthony <aa20.arunantony@gmail.com>
2026-04-15 16:37:56 +0530

    Trained from Scratch (No Pre-training) baseline with hpylori fine tune data

--- chemprop_train.sh
+++ chemprop_train.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 chemprop train \
-  --data-path data/processed/pre_train_ext.csv \
+  --data-path data/processed/fine_tune_ext.csv \
   --task-type classification \
   --smiles-columns smiles \
   --target-columns activity \
   --molecule-featurizers v1_rdkit_2d_normalized \
-  --save-dir models/pretrain_v6_rdkit \
-  --epochs 20
+  --save-dir models/finetune_v6_scratch_scaffold \
+  --epochs 50 \
+  --class-balance \
+  --split scaffold_balanced \
+  --num-replicates 10
